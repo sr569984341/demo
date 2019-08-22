e31f95f04ecbef2371a3cbf9ffde65376fbe3359
sr569984341 <sun_rui_369@163.com>
2019-08-22 16:21:43 +0800

    abc

--- src/main/java/com/example/pojo/Dept.java
+++ src/main/java/com/example/pojo/Dept.java
@@ -4,6 +4,7 @@ public class Dept {
     private int deptId;
     private String deptName;
     private int Id;
-    prateri int age;
+    private int age;
     private int IDD;
+    private int String;
 }
